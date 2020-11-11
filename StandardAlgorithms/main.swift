4bc50dc716bd5404c72a3d94750f55a6d591dbf5
Zachary Marinov <zacharymarinov@zacharys-macbook-air-3.local>
2020-11-11 11:43:14 -0600

    Initial Commit

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Zachary Marinov on 11/11/20.
+//  Copyright © 2020 VonNeumannGauss. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
